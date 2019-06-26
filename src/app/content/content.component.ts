98e5c0ecffc236dd96d629ca796ca53b8259e724
Andreas Hock <andreashock@bluewin.ch>
2019-06-26 20:56:03 +0000

    content log entry

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ContentService } from '../content/content.service';
+import { UserService } from '../user/user.service';
 
 @Component({
   selector: 'app-content',
@@ -6,10 +9,54 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class ContentComponent implements OnInit {
+  private contentid: string;
 
-  constructor() { }
+  constructor(private route:ActivatedRoute, private contents: ContentService, private users:UserService) {
+    this.route.params.subscribe( params => {
+      console.log("Content Param: ", params)
+      this.contentid = params.id;
+      this.contents.getContent(this.contentid);
+    })    
+  }
 
   ngOnInit() {
   }
 
+  public getLogoUrl(type: string): string {
+    var url = "";
+    console.log("Type:",type);
+    switch(type) {
+      case "1":
+        console.log("1");
+        url = "/assets/YouTube_logo_2017.png";
+        break;
+      case '2':
+        url = "/assets/tutorial.png";
+        break;
+      case '3':
+        url = "/assets/YouTube_logo_2017.png";
+        break;
+      case '4':
+        url = "/assets/YouTube_logo_2017.png";
+        break;
+      case '5':
+        url = "/assets/YouTube_logo_2017.png";
+        break;
+      case '6':
+        url = "/assets/YouTube_logo_2017.png";
+        break;
+      case '7':
+        url = "/assets/book.png";
+        break;
+      case '8':
+        url = "/assets/YouTube_logo_2017.png";
+        break;
+      case '9':
+        url = "/assets/YouTube_logo_2017.png";
+        break;
+      default:
+        url = "/assets/nomedia.png";
+    }
+    return url;
+  }
 }
--- src/app/content/content.service.ts
+++ src/app/content/content.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { UserService } from '../user/user.service';
+import { StatusService } from '../status/status.service';
+import { LogService, Log } from '../log/log.service';
+import { APP_CONFIG } from '../app-variables';
+
+export interface Content {
+  _id: string; 
+  name?: string;
+  
+}
 
 @Injectable({
   providedIn: 'root'
 })
-export class ContentService {
 
-  constructor() { }
+export class ContentService {
+  public content: Content;
+  
+  constructor(private user:UserService, private status:StatusService, private log:LogService, private http:HttpClient) { }
+  
+  
+  public getContent(id: string): void {
+    console.log("Content to load", id, this.content);
+    if(id) {
+      this.http
+        .get(APP_CONFIG.storageURL+"/api/0.1.0/content/get", {params:{id: id}})
+        .subscribe((data) => {
+          if( data['success'] ) {
+            this.content = <Content>data['content'];
+            console.log("Content loaded", this.content);
+          }
+        });
+    } 
+  }
+  public visitContent(): void {
+    console.log("Content "+this.content.name+" visited");
+    
+    this.log.createLog(<Log>{token: this.user.getUserToken(), message: "Lernmittel", type: 2, area: "content", content: "Lernmittel "+this.content.name+"wird verwendet ("+this.content._id+")"}); 
+  }
 }
