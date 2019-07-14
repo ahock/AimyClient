cbc405aea84e6151ea84179ebe1ddc4be4c54675
Andreas Hock <andreashock@bluewin.ch>
2019-07-14 10:49:07 +0000

    new challenge types

--- src/app/auth-service/auth0-variables.ts
+++ src/app/auth-service/auth0-variables.ts
@@ -7,6 +7,6 @@ interface AuthConfig {
 export const AUTH_CONFIG: AuthConfig = {
   clientID: 'Mf5V73R0MsibOggb2306fpDknAcuITs3',
   domain: 'iod.eu.auth0.com',
-  callbackURL: 'http://aimy.iod4all.com:8080/comp1'
-//  callbackURL: 'http://aimyc.s3-website.eu-central-1.amazonaws.com'
+//  callbackURL: 'http://aimy.iod4all.com:8080/comp1'
+  callbackURL: 'http://aimyc.s3-website.eu-central-1.amazonaws.com'
 };
--- src/app/challenge/challenge.service.ts
+++ src/app/challenge/challenge.service.ts
@@ -8,13 +8,15 @@ export interface Challenge {
     text: string,
     hint: string,
     type: string,
+    status: number, //0: ...
     lang: string,
     aspect: string,
     field: string,
     module: string,
     eduobjectives: [string],
     correct: [string],
-    answers: [string]
+    answers: [string],
+    participants: [string]
 }
 
 @Injectable({
@@ -33,8 +35,23 @@ export class ChallengeService {
       this.http
         .get(APP_CONFIG.storageURL+"/api/0.0.1/challenge/get", {params:{id: ida}})
         .subscribe((data) => {
-          console.log("Challenges loaded:", <[Challenge]>data);
+//          console.log("Challenges loaded:", <[Challenge]>data);
           this.challenges = <[Challenge]>data;
+          for(var i=0; i<this.challenges.length; i++) {
+            this.challenges[i].status = 0;
+            if(this.challenges[i].type[0]=="survey") {
+              console.log("Survey:", this.challenges[i]['survey']['numberparticipants']);
+            
+              for(var j=0;j<this.challenges[i]['survey']['numberparticipants'];j++) {
+                if(j==0) {
+                  this.challenges[i].participants = ["participant email address"];
+                } else {
+                  this.challenges[i].participants.push("participant email address"); 
+                }
+              }
+            }
+          }
+          console.log("Challenges loaded:", this.challenges);
         });
     }
   }
